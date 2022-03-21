7c7f41d1d6aa29dd42c9f59015f2cf97ce949922
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-21 22:50:04 +0600

    json load data ui

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
+                <a href="/shop">Shop</a>
                 <a href="/order">Order</a>
                 <a href="/review">Review</a>
                 <a href="/inventory">Inventory</a>
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                <h1>This is Shop: {products.length}</h1>
+            </div>
+            <div className="summary-container">
+                <h2>Order Summary</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
